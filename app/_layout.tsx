a11f559896d92679d0cc50fd4891a2adbfe258c6
Claire Lee <jclairelee2@gmail.com>
2025-12-01 10:49:33 -0800

    wrap app with messages provider and hide chat stack header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import { MessagesProvider } from "./context/MessagesContext";
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -10,11 +10,13 @@ export const unstable_settings = {
 export default function RootLayout() {
 
   return (
-    <>
+
+   <MessagesProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="chat/[id]" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
-    </>
+  </MessagesProvider>
   );
 }
